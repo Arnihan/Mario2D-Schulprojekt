41e6824c9a1002b2aec75fc2b594e6d07b77cefa
Arnihan <arnihan.manivannan123@gmail.com>
2024-04-16 13:22:57 +0200

    test

--- code/main.py
+++ code/main.py
@@ -1,20 +1,19 @@
-from settings import * 
+from settings import *
 
 class Game:
-	def __init__(self):
-		pygame.init()
-		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-		pygame.display.set_caption('Super Pirate World')
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Super Pirate World')
 
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
+    def run(self):
+        while True:
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-			pygame.display.update()
+          pygame.display.update()
 
-if __name__ == '__main__':
-    game = Game()
-    game.run()
+game = Game()
+game.run()
